a1e15e0e53630b5536b66d84eedab0007472010b
Sidd-dmg <102126686+Sidd-dmg@users.noreply.github.com>
2024-05-10 23:34:42 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,30 +1,31 @@
 import math
 
-# Step 1: Choose two prime numbers
-p = 7
+# Step 1: Choose prime numbers
+p = 3
 q = 5
 
 # Step 2: Calculate n
 n = p * q
 print("n =", n)
 
-# Step 3: Calculate phi(n)
+# Step 3: Calculate (phi)
 phi = (p - 1) * (q - 1)
 
-# Step 4: Find e such that 1 < e < phi(n) and gcd(e, phi) = 1
+# Step 4: Find a suitable value for e
 e = 2
 while e < phi:
-    if math.gcd(e, phi) == 1:
+    if math.gcd(e, phi) == 1:  # Check if e and phi are coprime
         break
     else:
         e += 1
 print("e =", e)
 
-# Step 5: Calculate d such that (d * e) mod phi(n) = 1
-d = pow(e, -1, phi)
+# Step 5: Calculate the private key (d)
+k = 4 # Arbitrarily chosen value for k
+d = ((k * phi) + 1) // e  # Calculate d using the extended Euclidean algorithm
 print("d =", d)
 
-# Display keys
+# Display public and private keys
 print(f'Public key: {e, n}')
 print(f'Private key: {d, n}')
 
@@ -33,9 +34,9 @@ msg = 4
 print(f'Original message: {msg}')
 
 # Encryption
-C = pow(msg, e, n)
+C = pow(msg, e, n)  # Ciphertext C = (msg^e) mod n
 print(f'Encrypted message: {C}')
 
 # Decryption
-M = pow(C, d, n)
+M = pow(C, d, n)  # Decrypted message M = (C^d) mod n
 print(f'Decrypted message: {M}')
